2fec3adeddcd95a476cf807c67a9ac274c184d4a
Josefa <jitramon@miuandes.cl>
2020-03-29 17:20:28 -0300

    firt commit

---
+++ Laboratorio 2 - Josefa Tramon/Laboratorio 2 - Josefa Tramon/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Laboratorio_2___Josefa_Tramon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
