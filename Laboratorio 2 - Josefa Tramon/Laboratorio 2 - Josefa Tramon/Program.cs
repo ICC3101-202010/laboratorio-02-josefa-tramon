92df0b918f95930f35c8e622c3bcc5ee3e4a60cc
Josefa <jitramon@miuandes.cl>
2020-03-31 14:19:38 -0300

    seguimos trabajando

--- Laboratorio 2 - Josefa Tramon/Laboratorio 2 - Josefa Tramon/Program.cs
+++ Laboratorio 2 - Josefa Tramon/Laboratorio 2 - Josefa Tramon/Program.cs
@@ -9,4 +9,77 @@ namespace Laboratorio_2___Josefa_Tramon
             Console.WriteLine("Hello World!");
         }
     }
-}
+    class Song
+    {
+        private string name;
+
+        private string album;
+
+        private string gender;
+
+        private string artist;
+
+
+        public Song(string name, string album, string artist, string gender)
+
+        {
+            this.name = name;
+
+            this.album = album;
+
+            this.artist = artist;
+
+            this.gender = gender;
+
+        }
+    }
+    class Espotifai 
+    {
+        public Song[] arraySongs;
+        public int num;
+
+        public Espotifai()
+        {
+            this.num = 0;
+
+        }
+        public bool AddSong(Song song)
+        {
+            this.arraySongs[num] = song;
+
+            this.num += 1;
+            return true;
+        }
+        public void seeSong()
+        {
+            foreach (Song song in this.arraySongs)
+            {
+                string nonmbre = song.name;
+                Console.Write("genero: " + song.gender + ", artista: " + song.artist + ", album: " + song.album + ", nombre: " + song.name + "\n");
+            }
+                
+        }
+
+
+
+
+
+    }
+
+
+        
+
+
+        
+
+    }
+
+
+    }
+
+
+
+}   
+
+
+    
